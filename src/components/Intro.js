3e4ec9475a790e90a174227e8e690a645f2db5c0
emagweb <86436236+emagweb@users.noreply.github.com>
2022-06-28 02:05:31 +0300

    Update Intro.js

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -10,7 +10,7 @@ function Intro() {
         <div className="intro__content">
           <div className="pv-40"></div>
           <h3 className="intro__subtitle flag">We are building a digital world<img src={flag} alt="flag" /></h3>
-          <h2 className="intro__title">Find the best software team<br />from Ukraine</h2>
+          <h2 className="intro__title">We are a software team<br />from Ukraine</h2>
           <div className="pv-40"></div>
           <a className="button-white" href={formUrl}>Free Inquiry</a>
           <div className="pv-40"></div>
